066ef7d1b6b252bd785a5f071b640ad82e6d7edc
unpete <info@oknosoft.ru>
2025-01-31 17:48:46 +0500

    локальный Autocomplete

--- src/DataField/Autocomplete.js
+++ src/DataField/Autocomplete.js
@@ -2,23 +2,16 @@ import React from 'react';
 import MuiAutocomplete from '../Autocomplete';
 import StyledInput from './StyledInput';
 
-const getOptionLabel = (v) => v?.presentation || v?.name || v?.toString() || '';
-
 /**
  * @summary Висящее в воздухе поле ввода
  * @desc Это не совсем DataField.
  * Он не редактирует DataObj, но позволяет показать и выбрать из списка, значение DataObj
  */
-export default function Autocomplete({label, fullWidth, disableClearable, placeholder, labelProps, noBorder, onClick, ...other}) {
+export default function Autocomplete({label, fullWidth, placeholder, labelProps, noBorder, onClick, ...other}) {
 
-  if(typeof disableClearable !== 'boolean') {
-    disableClearable = true;
-  }
 
   return <MuiAutocomplete
-    disableClearable={disableClearable}
-    getOptionLabel={getOptionLabel}
-    renderInput={(params) => <StyledInput {...params} labelProps={labelProps} fullWidth={fullWidth} label={label} placeholder={placeholder} noBorder={noBorder} onClick={onClick}/>}
+    renderInput={(params) => <StyledInput {...params} label={label} fullWidth={fullWidth} placeholder={placeholder} labelProps={labelProps}  noBorder={noBorder} onClick={onClick}/>}
     //renderOption={(props, option, state) => <Typography key={option.ref} noWrap>{option.name}</Typography>}
     {...other}
   />;
